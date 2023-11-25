d746bb0d00a8652b9ddd95b11d0c0df9370198ea
John Keikoglou <johnkeik@192.168.1.4>
2023-11-25 19:43:02 +0200

    - blog initialization

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -6,8 +6,35 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrl: './blog.component.scss',
 })
 export class BlogComponent {
+  expanded1 = false;
+  expanded2 = false;
+  expanded3 = false;
 
+  toggle1() {
+    console.log('here');
+    this.expanded1 = !this.expanded1;
+    this.expanded2 = false;
+    this.expanded3 = false;
+
+    window.scrollTo(0, 0);
+  }
+  toggle2() {
+    console.log('here');
+    this.expanded2 = !this.expanded2;
+    this.expanded1 = false;
+    this.expanded3 = false;
+
+    window.scrollTo(0, 0);
+  }
+  toggle3() {
+    console.log('here');
+    this.expanded3 = !this.expanded3;
+    this.expanded1 = false;
+    this.expanded2 = false;
+
+    window.scrollTo(0, 0);
+  }
 }
