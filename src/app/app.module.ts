ed3adfe952c20ca81f91e49de9ef93490d593560
John Keikoglou <johnkeik@192.168.1.4>
2023-11-26 18:07:27 +0200

    email form initialization minor text fixes testimonial images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {
-  TranslateLoader,
-  TranslateModule,
-  TranslateService,
-} from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { AppComponent } from './app.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { NgForm, FormsModule } from '@angular/forms';
+import { LoaderComponent } from '../loader/loader.component';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, LoaderComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  user_name: string = '';
+  user_email: string = '';
+  message: string = '';
 
+  informativeMessage: string = '';
+  messageType: string = '';
+  emailLoading: boolean = false;
+
+  onInputChange() {
+    this.informativeMessage = '';
+    this.messageType = '';
+  }
+  public sendEmail(e: Event, contactForm: NgForm) {
+    this.informativeMessage = '';
+    this.messageType = '';
+    this.emailLoading = true;
+    emailjs
+      .sendForm(
+        'service_ci76d1g',
+        'template_zhtwxhy',
+        e.target as HTMLFormElement,
+        'Qg2y-7Eq6_F6KqH3N'
+      )
+      .then(
+        () => {
+          this.informativeMessage = 'Το μήνυμα στάλθηκε επιτυχώς!';
+          this.messageType = 'success';
+          contactForm.reset();
+          this.emailLoading = false;
+          console.log(this.informativeMessage);
+        },
+        (error) => {
+          this.informativeMessage =
+            'Υπήρξε κάποιο πρόβλημα. Παρακαλώ δοκιμάστε ξανά!';
+          this.messageType = 'error';
+          this.emailLoading = false;
+        }
+      );
+  }
 }
